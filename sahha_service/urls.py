e37369e42d155d02e8dc223734bd5b337d7e2f11
laitassou <lhoussain.aitassou@gmail.com>
2022-08-30 21:31:22 +0200

    add login page

--- sahha_service/urls.py
+++ sahha_service/urls.py
@@ -18,5 +18,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # User management
+    path('accounts/', include('django.contrib.auth.urls')), # new
+
+    path('accounts/', include('users.urls')), # new
     path('', include('pages.urls')),
 ]
