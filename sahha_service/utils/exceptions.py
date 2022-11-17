ef0ae774acc24ce8b12b203f9108bd6c569b8a28
Prakhar B <prakhar1812.b@gmail.com>
2022-11-17 12:07:06 +0530

    Added contancts and exceptions

--- sahha_service/utils/exceptions.py
+++ sahha_service/utils/exceptions.py
@@ -1,2 +1,17 @@
 from rest_framework import status
 from sahha_service.utils import constants
+class CustomException(Exception):
+
+    def __init__(self, message="Something went wrong.", status=status.HTTP_400_BAD_REQUEST, code=constants.ERRORS.INTERNAL_ERROR):
+        super().__init__()
+        self.message = message
+        self.status = status
+        self.code = code
+
+
+    def missingFields(missings):
+	    return CustomException(
+	        message='Missing [' + '] or ['.join([', '.join(x) for x in missings]) + ']',
+	        status=status.HTTP_400_BAD_REQUEST,
+	        code=constants.ERRORS.MISSING_DATA
+	    )
